a9f0cf983ba16bd4c2cf1bf7f61a3a28ac9b85d4
Judith E <judith.edhogbo@gmail.com>
2020-08-10 15:37:26 +0100

    Working Calculator Exercise

--- src/main/java/com/qa/day2/Exercises.java
+++ src/main/java/com/qa/day2/Exercises.java
@@ -1,11 +1,15 @@
 package com.qa.day2;
 
 public class Exercises {
+
+    public static void main(String[]args){
+        int first=2;
+        int second=4;
+        int result=sum(first,second);
+    }
+    public static int sum(int firstNumber, int secondNumber ){
+        return (firstNumber+secondNumber);
+    }
 };
 
-public int sum(int firstNumber, int secondNumber ){
-    return (firstNumber+secondNumber);
-        };
-public static void main(String[]args){
 
-}
