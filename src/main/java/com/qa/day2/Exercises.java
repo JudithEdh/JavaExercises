991cb9f90141b118e90b9f5b0588cc509e239466
Judith E <judith.edhogbo@gmail.com>
2020-08-10 15:27:36 +0100

    Created Calculator Exercise

---
+++ src/main/java/com/qa/day2/Exercises.java
@@ -0,0 +1,11 @@
+package com.qa.day2;
+
+public class Exercises {
+};
+
+public int sum(int firstNumber, int secondNumber ){
+    return (firstNumber+secondNumber);
+        };
+public static void main(String[]args){
+
+}
