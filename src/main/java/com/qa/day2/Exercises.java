9a3712b033ad9b643433300e0cf57cfaf68fb6c1
Judith E <judith.edhogbo@gmail.com>
2020-08-10 15:42:15 +0100

    created subtract, multiply and divide method

--- src/main/java/com/qa/day2/Exercises.java
+++ src/main/java/com/qa/day2/Exercises.java
@@ -5,10 +5,23 @@ public class Exercises {
     public static void main(String[]args){
         int first=2;
         int second=4;
-        int result=sum(first,second);
+        int sum=sum(first,second);
+        int subtract=subtract(first,secod);
+        int divide=divide(first, second);
+        int multiply=multiply(first,second);
+
     }
     public static int sum(int firstNumber, int secondNumber ){
         return (firstNumber+secondNumber);
+    }
+    public static int subtract(int firstNumber, int secondNumber ){
+        return (firstNumber+secondNumber);
+    }
+    public static int multiply(int firstNumber, int secondNumber ){
+        return (firstNumber*secondNumber);
+    }
+    public static int divide(int firstNumber, int secondNumber ){
+        return (firstNumber/secondNumber);
     }
 };
 
