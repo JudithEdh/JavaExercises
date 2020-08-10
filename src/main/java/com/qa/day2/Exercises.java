e75c8c0a7710849cde0fd7e173cdc42e8386e520
Judith E <judith.edhogbo@gmail.com>
2020-08-10 15:49:22 +0100

    Printed the results

--- src/main/java/com/qa/day2/Exercises.java
+++ src/main/java/com/qa/day2/Exercises.java
@@ -6,16 +6,20 @@ public class Exercises {
         int first=2;
         int second=4;
         int sum=sum(first,second);
-        int subtract=subtract(first,secod);
+        int subtract=subtract(first,second);
         int divide=divide(first, second);
         int multiply=multiply(first,second);
+        System.out.println(first +" + "+ second+" = "+ sum);
+        System.out.println(first +" - "+ second+" = "+ subtract);
+        System.out.println(first +" * "+ second+" = "+ multiply);
+        System.out.println(first +" / "+ second+" = "+ divide);
 
     }
     public static int sum(int firstNumber, int secondNumber ){
         return (firstNumber+secondNumber);
     }
     public static int subtract(int firstNumber, int secondNumber ){
-        return (firstNumber+secondNumber);
+        return (firstNumber-secondNumber);
     }
     public static int multiply(int firstNumber, int secondNumber ){
         return (firstNumber*secondNumber);
