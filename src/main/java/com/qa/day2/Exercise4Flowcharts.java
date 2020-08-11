f3b1a607cd62c8388360087f5c435806ffa8f8a1
Judith E <judith.edhogbo@gmail.com>
2020-08-11 10:51:38 +0100

    First branch completed

---
+++ src/main/java/com/qa/day2/Exercise4Flowcharts.java
@@ -0,0 +1,12 @@
+package com.qa.day2;
+
+public class Exercise4Flowcharts {
+    public static void flow(int A){
+        if(A>2000){
+            System.out.println("A");
+            if(A>6000){
+
+            }
+        }
+    }
+}
