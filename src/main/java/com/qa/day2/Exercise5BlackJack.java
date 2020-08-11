b282b802a0d48f68c4742b77eb932050b7b9bc15
Judith E <judith.edhogbo@gmail.com>
2020-08-11 11:31:30 +0100

    Completed

---
+++ src/main/java/com/qa/day2/Exercise5BlackJack.java
@@ -0,0 +1,27 @@
+package com.qa.day2;
+
+public class Exercise5BlackJack {
+  public static int play(int a, int b){
+      int closeA= 21-a;
+      int closeB=21-b;
+
+      if((closeA>= 0)&&(closeB>=0)){
+          if(closeA>=closeB){
+              return b;
+          }else{
+              return a;
+          }
+      }else{
+          return 0;
+      }
+  }
+
+
+
+    public static void main(String[]args){
+        int result= play(24, 24);
+        System.out.println(result);
+    }
+
+
+}
