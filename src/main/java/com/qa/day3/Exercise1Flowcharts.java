1a9d24404cc10e981f752ffb5da13eb8fa5be6d3
Judith E <judith.edhogbo@gmail.com>
2020-08-11 11:38:43 +0100

    Completed first flowchart

---
+++ src/main/java/com/qa/day3/Exercise1Flowcharts.java
@@ -0,0 +1,9 @@
+package com.qa.day3;
+
+public class Exercise1Flowcharts {
+    public static void main(String[]args){
+        for (int A=100; A<=200;A++){
+           System.out.println(A);
+        }
+    }
+}
