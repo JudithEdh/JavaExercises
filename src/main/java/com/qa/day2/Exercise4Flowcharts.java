95040d7a8e4fe4c91ec6539e0caf402af8139cca
Judith E <judith.edhogbo@gmail.com>
2020-08-11 10:58:18 +0100

    branch A greater than 2000 completed

--- src/main/java/com/qa/day2/Exercise4Flowcharts.java
+++ src/main/java/com/qa/day2/Exercise4Flowcharts.java
@@ -5,7 +5,14 @@ public class Exercise4Flowcharts {
         if(A>2000){
             System.out.println("A");
             if(A>6000){
-
+                System.out.println("C");
+            }else{
+                System.out.println("B");
+                if(A>4000){
+                    System.out.println("D");
+                }else{
+                    System.out.println("E");
+                }
             }
         }
     }
