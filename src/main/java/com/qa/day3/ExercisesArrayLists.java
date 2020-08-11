50d8deb8ca950e27da32f8a9396078e64d54e450
Judith E <judith.edhogbo@gmail.com>
2020-08-11 15:18:50 +0100

    created array list

---
+++ src/main/java/com/qa/day3/ExercisesArrayLists.java
@@ -0,0 +1,17 @@
+package com.qa.day3;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExercisesArrayLists {
+
+    public static List<String> music = new ArrayList<>();
+    public static void main(String[]args) {
+        music.add("song1");
+        music.add("song2");
+        music.add("song3");
+        music.add("song4");
+        System.out.println(music);
+    }
+}
