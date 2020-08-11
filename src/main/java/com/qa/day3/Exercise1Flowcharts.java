69a6b03d52ed23a1073c38c57cd98a4cc00944b5
Judith E <judith.edhogbo@gmail.com>
2020-08-11 11:52:02 +0100

    Completed

--- src/main/java/com/qa/day3/Exercise1Flowcharts.java
+++ src/main/java/com/qa/day3/Exercise1Flowcharts.java
@@ -1,6 +1,24 @@
 package com.qa.day3;
 
 public class Exercise1Flowcharts {
+    //prints numbers 1-10 10 times each
+    public static void number(){
+       for(int i=1; i<=10; i++){
+           for(int j=0; j<10; j++){
+               System.out.println(i);
+           }
+       }
+    }
+    //prints number as many times as the value of that number
+    public static void number2(){
+        for(int i=1; i<=10; i++) {
+            for(int j=0; j<i; j++){
+                System.out.println(i);
+            }
+        }
+        }
+
+
     public static void main(String[]args){
         //flowchart 1
         for (int A=100; A<=200;A++){
@@ -14,6 +32,7 @@ public class Exercise1Flowcharts {
                 System.out.println("*");
             }
         }
-
+        number();
+        number2();
     }
 }
