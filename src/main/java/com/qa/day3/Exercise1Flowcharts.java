03d91881f98f09299edc0ece15f87abecf8f5e69
Judith E <judith.edhogbo@gmail.com>
2020-08-11 11:43:53 +0100

    Completed second flowchart

--- src/main/java/com/qa/day3/Exercise1Flowcharts.java
+++ src/main/java/com/qa/day3/Exercise1Flowcharts.java
@@ -2,8 +2,18 @@ package com.qa.day3;
 
 public class Exercise1Flowcharts {
     public static void main(String[]args){
+        //flowchart 1
         for (int A=100; A<=200;A++){
            System.out.println(A);
         }
+       //flowchart 2
+        for (int A=100;A<=200; A++){
+            if((A%2)==0){
+                System.out.println("-");
+            }else{
+                System.out.println("*");
+            }
+        }
+
     }
 }
