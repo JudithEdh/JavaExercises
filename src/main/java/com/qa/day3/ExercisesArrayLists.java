54b7241a5630b0b2e90afe9e25563bf91cf80045
Judith E <judith.edhogbo@gmail.com>
2020-08-11 15:50:45 +0100

    kind of completed

--- src/main/java/com/qa/day3/ExercisesArrayLists.java
+++ src/main/java/com/qa/day3/ExercisesArrayLists.java
@@ -2,11 +2,13 @@ package com.qa.day3;
 
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ExercisesArrayLists {
 
     public static List<String> music = new ArrayList<>();
+
     public static void main(String[]args) {
         music.add("song1");
         music.add("song2");
@@ -16,5 +18,16 @@ public class ExercisesArrayLists {
         for(int i=0; i<music.size(); i++){
             System.out.println(music.get(i));
         }
+        music.set(2,"song2 new");
+        System.out.println(music);
+        music.remove(1);
+        System.out.println(music);
+        music.add("song5");
+        music.add("song6");
+        music.add("song7");
+        music.add("song8");
+        System.out.println(music);
+
+
     }
 }
