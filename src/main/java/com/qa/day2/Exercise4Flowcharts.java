c1d7f44c9884e9d3da566f700867c98ea1d4eaff
Judith E <judith.edhogbo@gmail.com>
2020-08-11 11:21:38 +0100

    Completed

--- src/main/java/com/qa/day2/Exercise4Flowcharts.java
+++ src/main/java/com/qa/day2/Exercise4Flowcharts.java
@@ -1,19 +1,43 @@
 package com.qa.day2;
 
 public class Exercise4Flowcharts {
-    public static void flow(int A){
-        if(A>2000){
+
+    public static void flow(int A) {
+        if (A > 2000) {
             System.out.println("A");
-            if(A>6000){
+            if (A > 6000) {
                 System.out.println("C");
-            }else{
+            } else {
                 System.out.println("B");
-                if(A>4000){
+                if (A > 4000) {
                     System.out.println("D");
-                }else{
+                } else {
                     System.out.println("E");
                 }
             }
+        } else {
+            System.out.println("1");
+            if (A > 100) {
+                System.out.println("3");
+                if (A > 600) {
+                    System.out.println("5");
+                } else {
+                    System.out.println("4");
+                    if (A > 500) {
+                        System.out.println("6");
+                    } else {
+                        System.out.println("7");
+                    }
+                }
+            } else {
+                System.out.println("2");
+            }
         }
+
+    }
+
+    public static void main(String[]args){
+
+        flow(100);
     }
 }
