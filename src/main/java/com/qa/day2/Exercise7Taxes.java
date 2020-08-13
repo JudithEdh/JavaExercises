a750f68cea5b4c885e807cfdab1be6d19585e10d
Judith E <judith.edhogbo@gmail.com>
2020-08-13 10:15:25 +0100

    completed method percentage in exercise 7

---
+++ src/main/java/com/qa/day2/Exercise7Taxes.java
@@ -0,0 +1,22 @@
+package com.qa.day2;
+
+public class Exercise7Taxes {
+static int percentage(int salary){
+    int percentage=0;
+   if(salary < 20000 && salary>=15000){
+       percentage=10;
+   }else if(salary < 30000 && salary>=20000){
+       percentage=15;
+   }else if(salary < 45000 && salary>=30000){
+       percentage=20;
+   }else if (salary>=45000){
+       percentage=25;}
+
+
+
+    return percentage;
+}
+    public static void main(String[]args){
+
+    }
+}
