33dbe047b4bbf7b0e4142705b410725fb297a93e
Judith E <judith.edhogbo@gmail.com>
2020-08-18 15:42:15 +0100

    Completed string manipulation exercise

--- src/main/java/com/qa/day3/StringManipulation.java
+++ src/main/java/com/qa/day3/StringManipulation.java
@@ -26,8 +26,43 @@ public class StringManipulation {
         }
         System.out.println((word.substring(nextWordPosition,i)));
     }
+
+    static void printWordsVertically2(String word){
+        int i=word.length();
+        int nextWordPosition=word.length();
+
+        while(i>0){
+            if((word.substring(i-1,i)).equals(" ")){
+                System.out.println((word.substring(i,nextWordPosition)));
+                nextWordPosition=i-1;
+            }
+            i--;
+
+        }
+        System.out.println((word.substring(i,nextWordPosition)));
+    }
+
+    static void findWord(String word, String word2){
+        int i=0;
+        int nextWordPosition=0;
+
+        while(i<word.length()){
+            if((word.substring(i,i+1)).equals(" ")){
+                if((word.substring(nextWordPosition,i)).equals(word2)){
+                System.out.println((word.substring(nextWordPosition,i)));
+                }
+                nextWordPosition=i+1;
+            }
+            i++;
+
+        }
+
+    }
+
     public static void main(String[] args) {
     System.out.printf("There are %d words %n",findNumberWords("3 jo words"));
     printWordsVertically("there are so many");
+    printWordsVertically2("there are so many");
+        findWord("love is something beautiful", "love");
     }
 }
