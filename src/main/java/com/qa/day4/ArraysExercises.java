224b0b948d45d986073ce18e71dd977a7183a0f4
Judith E <judith.edhogbo@gmail.com>
2020-08-19 09:54:10 +0100

    Completed first task of array exercise

---
+++ src/main/java/com/qa/day4/ArraysExercises.java
@@ -0,0 +1,12 @@
+package com.qa.day4;
+
+public class ArraysExercises {
+
+    public static void main(String[] args) {
+
+        int[] exercise = {1,2,3,4,5,6,7,8,9,10};
+        System.out.printf("%d %d %d %d %d %d %d %d %d %d",exercise[0], exercise[1], exercise[2], exercise[3], exercise[4],
+                exercise[5], exercise[6], exercise[7], exercise[8], exercise[9]);
+
+    }
+}
